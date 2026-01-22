b52da08ec52dba67aa2f71849f7aefb6fdf92069
dchampoev <danich04@abv.bg>
2026-01-22 13:27:22 +0200

    Add working TCP client

---
+++ client/src/client/ClientMain.java
@@ -0,0 +1,52 @@
+package client;
+
+import java.io.*;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class ClientMain {
+    private static final String HOST = "localhost";
+    private static final int PORT = 80;
+
+    static void main() {
+        try(
+                Socket socket = new Socket(HOST,PORT);
+                BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                PrintWriter out = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()),true);
+                Scanner scanner = new Scanner(System.in)
+                ){
+            System.out.println(in.readLine());
+
+            //LOGIN
+            System.out.print("Username: ");
+            String user =  scanner.nextLine();
+            System.out.print("Password: ");
+            String password = scanner.nextLine();
+
+            out.println("LOGIN "+user+" "+password);
+            String resp = in.readLine();
+            System.out.println(resp);
+
+            if(!resp.startsWith("OK")){
+                return;
+            }
+
+            //COMMAND LOOP
+            while(true){
+                System.out.print("> ");
+                String cmd =  scanner.nextLine().trim();
+                if(cmd.isEmpty()) continue;
+
+                out.println(cmd);
+                String serverResp = in.readLine();
+                System.out.println(serverResp);
+
+                if(cmd.equalsIgnoreCase("QUIT")){
+                    break;
+                }
+            }
+        } catch(IOException e) {
+            System.err.println("Client error: "+e.getMessage());
+        }
+    }
+}
